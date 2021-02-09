1ab783bb11b883342507805257ca7650c2f60b20
Gillis Rikardsson <gillis.rikardsson@gmail.com>
2021-02-09 20:36:18 +0000

    five surfboards created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,21 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Starting seed...."
 
+gillis = User.create!(email: "gillis@mail.com", password: "123456")
 
-Surfboard.create(name: "X-5000", details: "Rent my pro board now", price: 15,
-  available: "Yes", location: "Venice Beach")
+Surfboard.create!(name: "X-5000", details: "Rent my pro board now", price: 15,
+  available: "Yes", location: "Venice Beach", user: gillis)
 
-Surfboard.create(name: "Oklay", details: "I got sick surfboard you can rent", price: 13,
-  available: "Yes", location: "Miami Beach")
+Surfboard.create!(name: "Oklay", details: "I got sick surfboard you can rent", price: 13,
+  available: "Yes", location: "Miami Beach", user: gillis)
 
-Surfboard.create(name: "Nike Boost", details: "Rent a perfect beginner board", price: 10,
-  available: "No", location: "Barcelona Beach")
+Surfboard.create!(name: "Nike Boost", details: "Rent a perfect beginner board", price: 10,
+  available: "No", location: "Barcelona Beach", user: gillis)
 
-Surfboard.create(name: "Le Wagon", details: "Surf like you surf the web, super-easy", price: 19,
-  available: "Yes", location: "Shorditch cannel")
+Surfboard.create!(name: "Le Wagon", details: "Surf like you surf the web, super-easy", price: 19,
+  available: "Yes", location: "Shorditch cannel", user: gillis)
 
-Surfboard.create(name: "Quicksilver Madness", details: "Advanced surfboard for pros", price: 25,
-  available: "No", location: "Tulum Beach")
+Surfboard.create!(name: "Quicksilver Madness", details: "Advanced surfboard for pros", price: 25,
+  available: "No", location: "Tulum Beach", user: gillis)
 
-
+puts "seeding done!"
