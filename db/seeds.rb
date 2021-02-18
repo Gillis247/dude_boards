b65a22c8d1846439339b798e0e56fe7437851aa8
George Ellery-Masson <59938892+gelllery2395@users.noreply.github.com>
2021-02-18 22:38:22 +0000

    Revert "card updates"

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,23 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 puts "Starting seed...."
 
-Gillis = User.create!(email: "gillis12@mail.com", password: "123456")
-Roberto = User.create!(email: "roberto12@mail.com", password: "123456")
+gillis = User.create!(email: "gillis12@mail.com", password: "123456")
+roberto = User.create!(email: "roberto12@mail.com", password: "123456")
 
 Surfboard.create!(name: "X-5000", details: "Rent my pro board now", price: 15,
-  available: true, location: "Venice Beach", user: Gillis)
+  available: true, location: "Venice Beach", user: gillis)
 
-Surfboard.create!(name: "Oklay", details: "I got a sick surfboard you can rent", price: 13,
-  available: true, location: "Miami Beach", user: Gillis)
+Surfboard.create!(name: "Oklay", details: "I got sick surfboard you can rent", price: 13,
+  available: true, location: "Miami Beach", user: gillis)
 
 Surfboard.create!(name: "Nike Boost", details: "Rent a perfect beginner board", price: 10,
-  available: false, location: "Barcelona Beach", user: Roberto)
+  available: false, location: "Barcelona Beach", user: roberto)
 
 Surfboard.create!(name: "Le Wagon", details: "Surf like you surf the web, super-easy", price: 19,
-  available: true, location: "Shorditch Canal", user: Roberto)
+  available: true, location: "Shorditch cannel", user: roberto)
 
 Surfboard.create!(name: "Quicksilver Madness", details: "Advanced surfboard for pros", price: 25,
-  available: false, location: "Tulum Beach", user: Roberto)
+  available: false, location: "Tulum Beach", user: roberto)
 
 
 puts "seeding done!"
