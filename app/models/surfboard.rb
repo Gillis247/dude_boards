9756df0b499af552751ec5c35e3ae98139813a57
oday abu saleh <adlioday@gmail.com>
2021-02-20 14:16:25 +0000

    frotn end changes

--- app/models/surfboard.rb
+++ app/models/surfboard.rb
@@ -3,7 +3,5 @@ class Surfboard < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_location?
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   has_one_attached :photo
-  #validates :name, :details, :price, :location, presence: true
-  #validates :name, uniqueness: true
-  #validates :available, inclusion: { in: [ true, false ] }
+  validates :name, :details, :price, :location, presence: true
 end
